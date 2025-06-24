7c2899f3a0c5a23dd3fbb9eb43d24453d545fc4c
mlr2905 <7585474@gmail.com>
2025-06-24 11:33:22 +0300

    Update PacManAnimationManager.java

--- src/view/animations/PacManAnimationManager.java
+++ src/view/animations/PacManAnimationManager.java
@@ -21,25 +21,25 @@ public class PacManAnimationManager {
 
     public void loadpacManImages() {
         try {
-            BufferedImage down1 = ImageIO.read(getClass().getResourceAsStream("/view/resources/pacMan/pacmanDown.png"));
-            BufferedImage down2 = ImageIO.read(getClass().getResourceAsStream("/view/resources/pacMan/pacmanClosed.png"));
-            BufferedImage right1 = ImageIO.read(getClass().getResourceAsStream("/view/resources/pacMan/pacmanRight.png"));
-            BufferedImage left1 = ImageIO.read(getClass().getResourceAsStream("/view/resources/pacMan/pacmanLeft.png"));
-            BufferedImage up1 = ImageIO.read(getClass().getResourceAsStream("/view/resources/pacMan/pacmanUp.png"));
+            BufferedImage down = ImageIO.read(getClass().getResourceAsStream("/view/resources/pacMan/pacmanDown.png"));
+            BufferedImage right = ImageIO.read(getClass().getResourceAsStream("/view/resources/pacMan/pacmanRight.png"));
+            BufferedImage left = ImageIO.read(getClass().getResourceAsStream("/view/resources/pacMan/pacmanLeft.png"));
+            BufferedImage up = ImageIO.read(getClass().getResourceAsStream("/view/resources/pacMan/pacmanUp.png"));
+            BufferedImage Closed = ImageIO.read(getClass().getResourceAsStream("/view/resources/pacMan/pacmanClosed.png"));
 
             frames = new BufferedImage[4][3];
-            frames[0][0] = down1;
-            frames[0][1] = down2;
-            frames[0][2] = down1;
-            frames[1][0] = right1;
-            frames[1][1] = down2;
-            frames[1][2] = right1;
-            frames[2][0] = left1;
-            frames[2][1] = down2;
-            frames[2][2] = left1;
-            frames[3][0] = up1;
-            frames[3][1] = down2;
-            frames[3][2] = up1;
+            frames[0][0] = down;
+            frames[0][1] = Closed;
+            frames[0][2] = down;
+            frames[1][0] = right;
+            frames[1][1] = Closed;
+            frames[1][2] = right;
+            frames[2][0] = left;
+            frames[2][1] = Closed;
+            frames[2][2] = left;
+            frames[3][0] = up;
+            frames[3][1] = Closed;
+            frames[3][2] = up;
 
             System.out.println("pacMan images loaded successfully!");
         } catch (Exception e) {
